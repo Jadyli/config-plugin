4164790d7259a4a477a1e99685ae9faf09c26a2d
sanqi <lipingfa@bilibili.com>
2022-09-29 16:47:10 +0800

    Fix bug of kapt.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.config.plugin)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
--- common/common/build.gradle.kts
+++ common/common/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.config.plugin)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 dependencies {
